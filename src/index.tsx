1b97b9f2b3f227c3efff1adab27ab29f2ef9b8ff
Marco Fontana <fontana.marco@hotmail.com>
2021-03-12 12:54:48 +0100

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,24 @@
-import { NativeModules } from 'react-native';
+import { NativeEventEmitter, NativeModules } from 'react-native';
 
 type WearEngineType = {
-  multiply(a: number, b: number): Promise<number>;
+  hasAvailableDevices(): void;
+  requestPermission(): void;
+  checkPermission(): void;
+  getDevices(): void;
+  getConnectedDevice(): void;
+  setAndPingConnectedDevice(peerPkgName: String, peerFingerPrint: String): void;
+  sendMessage(messageStr: String): void;
+  sendFile(filePath: String): void;
+  cancelFile(filePath: String): void;
+  addEventListener(event : String, listener : any) : void;
 };
 
 const { WearEngine } = NativeModules;
+
+const emitter = new NativeEventEmitter(WearEngine);
+
+WearEngine.addEventListener = function(event : string, listener : any) {
+    return emitter.addListener(event, listener);
+}
 
 export default WearEngine as WearEngineType;
